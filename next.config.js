0809c2eff455b0f9f8108c69bf31c4d2aaaa75f7
intiba <intiba@example.com>
2025-05-04 23:27:36 +0300

    feat: Add basic next.config.js

---
+++ next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // reactStrictMode: true, // İsteğe bağlı: Strict Mode'u etkinleştirebilirsiniz
+  // output: 'standalone', // Vercel genellikle bunu kendi yönetir, özel durumlar dışında gerekmez
+};
+
+module.exports = nextConfig;
